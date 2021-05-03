f8f3e0ee3bbff73407e33d22c11414e0c1c1a975
David Godinez <godinezjosue@hotmail.com>
2021-05-03 07:19:34 -0600

    Delete configs.js

--- configs/configs.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    llave: "this_key_is_ultra_secret_xdxdxd_123456",
-    string_conexion: "mongodb+srv://sa:14090310Jos@cluster0.qnbic.mongodb.net/DB_FIX_IT?retryWrites=true&w=majority"
-}
